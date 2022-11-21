f1e250238e60df338f60d6bf2c2fd36d555deb47
skippynea <53440878+skippynea@users.noreply.github.com>
2022-11-21 11:47:50 +1100

    express

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const express = require('express');
 
 
 const app = express();
-app.use((req,res)=>{
-    res.json({message:'your request was successful !' })
 
+app.use((req, res, next)=>{
+    console.log('Request received !');
+    next();
 })
 
+app.use((req, res, next)=>{
+    res.status(201);
+    next();
+})
 
+app.use((req, res, next)=>{
+    res.json({message:'your request was successful !' })
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log('Response sent successfully !');
+})
 
 module.exports = app;
