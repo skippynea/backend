209df3bedf0c89cce7b526cf689e96fc32ada730
skippynea <53440878+skippynea@users.noreply.github.com>
2023-02-06 11:43:49 +1100

    created a stuff controller

---
+++ controllers/stuff.js
@@ -0,0 +1,26 @@
+// in controllers/stuff.js
+
+const Thing = require('../models/thing');
+
+exports.createThing = (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
+const stuffCtrl = require('../controllers/stuff');
+
 const Thing = require('../models/thing');
 
-router.post('/', (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  thing.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+router.post('/', stuffCtrl.createThing);
 
 router.get('/:id', (req, res, next) => {
   Thing.findOne({
