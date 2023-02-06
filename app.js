3ab6dd7a193203f2e56f4bb706f741e74088dc73
skippynea <53440878+skippynea@users.noreply.github.com>
2023-02-06 11:33:09 +1100

    updated routes in app.js and stuff.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   });
 
 
-
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,108 +1,100 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 
-const Thing = require('../models/thing')
+const Thing = require('../models/thing');
 
-router.post('/api/products', (req, res, next) => {
-    console.log('post');
-    const thing = new Thing({
-        "name": req.body.name,
-  
-        "description": req.body.description,
-    
-        "price": req.body.price,
-    
-        "inStock": req.body.inStock
-    
-    });
-    
-    thing.save()
-    .then((product) => {
-        console.log('Thing has been saved !');
-        res.status(201).json({
-          product
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
+router.post('/', (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
   });
-  
-  router.get('/api/products/:id', (req, res, next) => {
-    Thing.findOne({
-      _id: req.params.id
-    }).then(
-      (product) => {
-        res.status(200).json({product});
-      }
-    ).catch(
-      (error) => {
-        res.status(404).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  
-  router.put('/api/products/:id', (req, res, next)=>{
-    const thing=new Thing({
-      _id: req.params.id,
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price,
-      inStock: req.body.inStock
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
-      () => {
-        res.status(201).json({
-          message: 'Modified!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  
-  router.delete('/api/products/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  
-  router.get('/api/products', (req, res, next) => {
-    Thing.find().then(
-      (products) => {
-        console.log(products)
-        res.status(200).json({products});
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
 
-module.exports= router;
+router.get('/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.put('/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.delete('/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.get('/' +
+  '', (req, res, next) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+module.exports = router;
